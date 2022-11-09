579a33881da4fe9aa145e0293a31762926c26bfd
DIGVIJAY <singhdigvijay703@gmail.com>
2022-11-10 02:37:16 +0530

    front end update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { container, AppBar, 
+import { container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import chat from './images/Chat.jpg'
 
 const App = ()=> {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <container maxidth='lg'>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Xenon-Chat</Typography>
+        <img src={chat} alt="xenon-chat" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Grid container justifyContent="space-between" alignItems="stretch" spacing="">
+          <Grid item xs={12} sm={7}>
+            <Posts />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form />
+          </Grid>
+        </Grid>
+      </Grow>
+    </container>
   );
 }
 
---
+++ src/components/Posts/posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from './Post/Post'
+
+const posts = () => {
+    return (
+        <>
+        <h1>Post</h1>
+        <Post />
+        <Post />
+        </>
+    );
+}
+ export default posts;
